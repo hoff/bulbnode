932a339fa47b38b9c12d5d75d5be6b89a9c14f80
hoff <micklinghoff@gmail.com>
2016-10-11 14:46:27 +0200

    bleno for periphery

--- bulbnode.js
+++ bulbnode.js
@@ -3,11 +3,39 @@ var express = require('express')();
 var http = require('http').Server(express);
 var io = require('socket.io')(http);
 var noble = require('noble');
+// try bleno
+var bleno = require('bleno');
+var BlenoPrimaryService = bleno.PrimaryService;
+var EchoCharacteristic = require('./characteristic');
+bleno.on('stateChange', function (state) {
+    console.log('on -> stateChange: ' + state);
+    if (state === 'poweredOn') {
+        bleno.startAdvertising('echo', ['ec00']);
+    }
+    else {
+        bleno.stopAdvertising();
+    }
+});
+bleno.on('advertisingStart', function (error) {
+    console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+    if (!error) {
+        bleno.setServices([
+            new BlenoPrimaryService({
+                uuid: 'ec00',
+                characteristics: [
+                    new EchoCharacteristic()
+                ]
+            })
+        ]);
+    }
+});
+// end of bleno
 /**
  * The Server
  */
 http.listen(3000, function () {
-    console.log('started scanning and listening on port 3000');
+    console.log('!!!started scanning and listening on port 3000');
+    console.log('bleno?', bleno);
 });
 /**
  * Express Routes
--- bulbnode.ts
+++ bulbnode.ts
@@ -5,11 +5,44 @@ var http = require('http').Server(express)
 var io = require('socket.io')(http)
 var noble = require('noble')
 
+// try bleno
+var bleno = require('bleno')
+var BlenoPrimaryService = bleno.PrimaryService;
+var EchoCharacteristic = require('./characteristic');
+
+bleno.on('stateChange', function(state) {
+  console.log('on -> stateChange: ' + state);
+
+  if (state === 'poweredOn') {
+    bleno.startAdvertising('echo', ['ec00']);
+  } else {
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function(error) {
+  console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+  if (!error) {
+    bleno.setServices([
+      new BlenoPrimaryService({
+        uuid: 'ec00',
+        characteristics: [
+          new EchoCharacteristic()
+        ]
+      })
+    ]);
+  }
+});
+
+// end of bleno
+
+
 /**
  * The Server
  */
 http.listen(3000, () => {
-    console.log('started scanning and listening on port 3000')
+    console.log('!!!started scanning and listening on port 3000')
+    console.log('bleno?', bleno)
 })
 
 /**
@@ -17,7 +50,7 @@ http.listen(3000, () => {
  */
 express.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
-    noble.startScanning()    
+    noble.startScanning()
 })
 
 /**
